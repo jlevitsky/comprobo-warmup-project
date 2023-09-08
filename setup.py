d22f0b1612edd2bfadae96952de177589c0e129e
Jack Levitsky <jlevitsky@olin.edu>
2023-09-08 14:31:32 -0400

    Fixed Catastrophic Failure

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'teleop = comprobo-warmup-project.teleop:main'
+            'teleop = comprobo_warmup_project.teleop:main'
         ],
     },
 )
